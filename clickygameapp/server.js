2bce15c229b49ddfed7cfefe12268b301604506c
admin <admin@admins-MacBook-Pro.local>
2020-02-27 00:38:25 -0800

    deployed to heroku

--- clickygameapp/server.js
+++ clickygameapp/server.js
@@ -3,21 +3,15 @@ const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 const routes = require("./routes");
-//const seeds = require("./scripts/seedDB")
-// Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-// Define API routes here
-// Add routes, both API and view
 app.use(routes);
 
 // Connect to the Mongo DB
 
 
-// Send every other request to the React app
-// Define any API routes before this runs
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
