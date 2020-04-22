8eb6da04fb3360fabfa0d79aa5374e193cd50be2
admin <admin@admins-MacBook-Pro.local>
2020-04-22 09:41:32 -0700

    Updates

---
+++ precache-manifest.efc99e87ba4e2375935f0a231e314a0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dbe49c3964264c5b308efacb02f7ab9",
+    "url": "/clicky-Game/index.html"
+  },
+  {
+    "revision": "84f865920627ad1775fa",
+    "url": "/clicky-Game/static/css/main.fe4096a3.chunk.css"
+  },
+  {
+    "revision": "1f1fab3f226ff50ae39e",
+    "url": "/clicky-Game/static/js/2.543bb12a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/clicky-Game/static/js/2.543bb12a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "84f865920627ad1775fa",
+    "url": "/clicky-Game/static/js/main.34079110.chunk.js"
+  },
+  {
+    "revision": "125bedcdc8e0d53a2c44",
+    "url": "/clicky-Game/static/js/runtime-main.83a0635e.js"
+  }
+]);
